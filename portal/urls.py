3cc40960c998bfcbf37e3f41944fc342ec1458b3
Choijoo Erdenesuren <e.choijoo0228@gmail.com>
2026-03-05 21:23:25 +0000

    event

--- portal/urls.py
+++ portal/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("events/", views.events_list, name="events_list"),
+    path("events/<int:event_id>/", views.event_detail, name="event_detail"),
     path("resources/", views.resources_list, name="resources_list"),
     path("suggest-event/", views.suggest_event, name="suggest_event"),
 ]
--- portal/views.py
+++ portal/views.py
@@ -1,5 +1,9 @@
+from time import timezone
+
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import Event
 
 # Create your views here.
 
@@ -7,7 +11,17 @@ def home(request):
     return render(request, "portal/home.html")
 
 def events_list(request):
-    return HttpResponse("Events page")
+    events = (
+        Event.objects
+        .filter(is_published=True)
+        .order_by("start_datetime")
+    )
+    return render(request, "portal/events_list.html", {"events": events})
+
+
+def event_detail(request, event_id):
+    event = get_object_or_404(Event, id=event_id, is_published=True)
+    return render(request, "portal/event_detail.html", {"event": event})
 
 def resources_list(request):
     return HttpResponse("Resources page")
