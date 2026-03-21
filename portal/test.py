75ff6898ba48d158483c7a1fb9f5392930bc88ea
Choijoo Erdenesuren <e.choijoo0228@gmail.com>
2026-03-21 13:42:05 +0000

    some small fix and ci test

---
+++ portal/test.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import EventSuggestion
+
+class EventSuggestionModelTest(TestCase):
+    
+    def test_create_event_suggestion(self):
+        user = User.objects.create_user(username='testuser', password='testpass')
+        
+        suggestion = EventSuggestion.objects.create(
+            user=user,
+            title='Test Event',
+            location='Dublin',
+            start_datetime='2030-07-01 10:00:00',
+            status = "PENDING"
+        )
+        self.assertEqual(suggestion.title, 'Test Event')
